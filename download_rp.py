2ddd94c3c396c58be661acfb317c62e095a96aa8
SAM <8dmasters@gmail.com>
2024-10-31 01:58:23 +0530

    Clear cache and reduced download files

--- download_rp.py
+++ download_rp.py
@@ -3,13 +3,15 @@ import os
 from arxiv import Client, Search, SortCriterion
 from pathlib import Path
 from tqdm import tqdm
+from utils.clear_download_cache import cleanup
 
 if __name__ == '__main__':
     curr_dir = Path(__file__).resolve().parent
-    total_papers = 800
+    download_dir = str(curr_dir / 'Papers')
+    total_papers = 500
 
-    if not os.path.exists(str(curr_dir / 'Papers')):
-        os.makedirs(str(curr_dir / 'Papers'))
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
 
     client = Client(
         page_size=total_papers
@@ -21,4 +23,5 @@ if __name__ == '__main__':
     )
 
     for paper in tqdm(client.results(search), total=total_papers):
-        paper.download_pdf(dirpath=str(curr_dir / 'Papers'))
+        paper.download_pdf(dirpath=download_dir)
+    cleanup()
---
+++ utils/clear_download_cache.py
@@ -0,0 +1,4 @@
+from urllib.request import urlcleanup
+
+def cleanup():
+    urlcleanup()
