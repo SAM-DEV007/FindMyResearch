8c2979cb417f13dc14e59d717af2f92d19c92a86
SAM <8dmasters@gmail.com>
2024-10-31 01:00:23 +0530

    Constraints for paper download

--- download_rp.py
+++ download_rp.py
@@ -1,16 +1,19 @@
 import os
+
 from arxiv import Client, Search, SortCriterion
 from pathlib import Path
 from tqdm import tqdm
 
 if __name__ == '__main__':
     curr_dir = Path(__file__).resolve().parent
-    total_papers = 1000
+    total_papers = 800
 
     if not os.path.exists(str(curr_dir / 'Papers')):
         os.makedirs(str(curr_dir / 'Papers'))
 
-    client = Client()
+    client = Client(
+        page_size=total_papers
+    )
     search = Search(
         query='all', 
         max_results=total_papers, 
