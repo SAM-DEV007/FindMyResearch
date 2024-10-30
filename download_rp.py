eb75f68df94e3be5d6ab718ac13a07b32a82bf07
SAM <8dmasters@gmail.com>
2024-10-30 23:55:12 +0530

    Download research papers

--- download_rp.py
+++ download_rp.py
@@ -1,9 +1,21 @@
 import os
-import arxiv
+from arxiv import Client, Search, SortCriterion
 from pathlib import Path
+from tqdm import tqdm
 
 if __name__ == '__main__':
-    curr_dir = Path(__file__).resovle().parent
+    curr_dir = Path(__file__).resolve().parent
+    total_papers = 1000
 
     if not os.path.exists(str(curr_dir / 'Papers')):
         os.makedirs(str(curr_dir / 'Papers'))
+
+    client = Client()
+    search = Search(
+        query='all', 
+        max_results=total_papers, 
+        sort_by = SortCriterion.Relevance
+    )
+
+    for paper in tqdm(client.results(search), total=total_papers):
+        paper.download_pdf(dirpath=str(curr_dir / 'Papers'))
