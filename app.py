16829bca223c536f21aad6454f9cc1d0013a3ab6
SAM <8dmasters@gmail.com>
2024-11-02 02:15:01 +0530

    Add context generation and saving

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ from utils import generate_metadata
 generate_metadata.generate_metadata_main()
 
 from SearchAlgorithms import aisearch, linear
+linear.generate_context()
 aisearch.load_pdf()
 aisearch.load_images()
 
 
 metadata = generate_metadata.load_metadata()
+context = linear.load_context()
 
 corpus = load_file('semantic_sentence.dat')
 main_images = load_file('semantic_image.dat')
