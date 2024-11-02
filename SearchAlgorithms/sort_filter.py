ee8481b53076276080ce2e0f63382ddd1ce54291
SAM <8dmasters@gmail.com>
2024-11-02 20:58:29 +0530

    Fixed typo and reduced sorting lines

--- SearchAlgorithms/sort_filter.py
+++ SearchAlgorithms/sort_filter.py
@@ -3,14 +3,10 @@ import re
 
 def sort_search(metadata: dict, results: list, field: str, order: int): # order - 0: increase, 1: decrease
     #title, author, keywords, date, publisher, abstract, doi
-    sorted_results = {}
-    for pdf in metadata:
-        sorted_results[pdf] = metadata[pdf][field]
-
     return list(sorted(results, key=lambda k: metadata[k][field], reverse=order))
 
 
-def sort_relevance(results: list, order: int): # order - 0: increase, 1: decrease
+def sort_relevance(results: dict, order: int): # order - 0: increase, 1: decrease
     # Only for semantic search
     return list(sorted(results, key=results.get, reverse=order))
 
