1d6a62d9171a17a109c3f231c2f61a3712d2e884
SAM <8dmasters@gmail.com>
2024-11-02 14:54:33 +0530

    Add basic semantic search

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import streamlit as st
 
-from utils import generate_metadata
-from SearchAlgorithms import aisearch, linear, sort_filter
+
+@st.cache_resource(show_spinner=False)
+def model_load():
+    temp_info = st.info('Loading and Verifying Models... Please wait...')
+
+    from utils import generate_metadata
+    from SearchAlgorithms import aisearch, linear, sort_filter
+
+    temp_info.empty()
+
+    return generate_metadata, aisearch, linear, sort_filter
 
 
-@st.cache_resource # st.cache_resource.clear()
-def start():
-    temp_info = st.info('Loading and Verifying Models and Cache... Please wait...')
+@st.cache_resource(show_spinner=False)
+def cache_load():
+    temp_info = st.info('Loading and Verifying Cache... Please wait...')
 
     generate_metadata.generate_metadata_main()
     linear.generate_context()
@@ -27,7 +36,14 @@ def start():
 
 st.title('FindMyResearch')
 
-metadata, context, corpus, main_images = start()
+generate_metadata, aisearch, linear, sort_filter = model_load()
+metadata, context, corpus, main_images = cache_load()
 
 search = st.text_input('Search')
+
+if search:
+    cache_load.clear()
+    cache_load()
+
+    search = aisearch.semantic_search(corpus, search)
 st.write('Search Results:', search)
